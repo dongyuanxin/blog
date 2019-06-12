27f92ead8eb46be3f34aa7bd6f02727861b01c89
dongyuanxin <yuanxin.me@gmail.com>
2019-06-12 16:06:08 +0800

    fix: plugin import error

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
   plugins: [
     require('./plugins/my-router'),
     [
-      require('./../../vuepress-plugin-comment/'),
+      require('vuepress-plugin-comment'),
       {
         choosen: 'gitalk', 
         options: {
