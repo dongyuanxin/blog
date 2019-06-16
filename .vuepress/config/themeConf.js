156b0cf61274b4258fbe295cee81b7027267d540
dongyuanxin <yuanxin.me@gmail.com>
2019-06-17 00:51:30 +0800

    change: 导航栏

--- .vuepress/config/themeConf.js
+++ .vuepress/config/themeConf.js
@@ -9,10 +9,30 @@ module.exports = {
   sidebar,
   nav: [
     { text: '导航', link: '/guide/' },
-    { text: '前端体系', link: '/passages/2019-03-26-javascript-first/' },
-    { text: '设计模式', link: '/passages/2018-10-23-singleton-pattern/' },
-    { text: 'Git手册', link: '/passages/2018-09-06-git-tag-and-version/' },
-    { text: '每周分享', link: '/passages/2019-06-10-weekly-share/' },
+    { 
+      text: '前端体系',
+      items: [
+        { text: 'JavaScript', link: '/passages/2019-03-26-javascript-first/' },
+        { text: 'ES6', link: '/passages/2019-04-09-es6/' },
+        { text: '浏览器与安全', link: '/passages/2019-05-15-browser/' },
+        { text: 'CSS', link: '/passages/2019-03-20-css3-flex/' },
+      ]
+    },
+    { 
+      text: '设计模式', 
+      items: [
+        { text: '创建型模式', link: '/passages/2018-10-23-singleton-pattern/' },
+        { text: '结构型模式', link: '/passages/2018-12-16-flyweight-pattern/' },
+        { text: '行为型模式', link: '/passages/2018-11-25-command-pattern/' },
+      ]
+    },
+    {
+      text: '笔记',
+      items: [
+        { text: '每周分享', link: '/passages/2019-06-10-weekly-share/' },
+        { text: 'Git手册', link: '/passages/2018-09-06-git-tag-and-version/' },
+      ]
+    },
     {
       text: '更多',
       items: [
