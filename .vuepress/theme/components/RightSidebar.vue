41d9ce878814878b5cf817090322a17427835135
yuanxindong <yuanxin.me@gmail.com>
2020-10-01 16:59:42 +0800

    feat: 更新右侧侧边栏

--- .vuepress/theme/components/RightSidebar.vue
+++ .vuepress/theme/components/RightSidebar.vue
@@ -2,7 +2,18 @@
     <aside class="right-sidebar">
         <div class="right-sidebar-links">
             <div class="right-sidebar-header">关注公众号</div>
-            <img src="https://static.godbmw.com/img/public/wechat-8cm.jpg" alt="公众号搜索：心谭博客" srcset />
+            <img
+                src="https://tva1.sinaimg.cn/large/006tNbRwly1gaknn8mr5ej3076076aaj.jpg"
+                alt="公众号搜索：心谭博客"
+                srcset
+            />
+        </div>
+
+        <div class="right-sidebar-links">
+            <div class="right-sidebar-header">
+                本文目录
+            </div>
+            <TOC />
         </div>
 
         <div class="right-sidebar-links">
@@ -10,30 +21,19 @@
                 最近更新
                 <a href="/guide/" target="_blank">&gt;&gt;&gt;查看全部</a>
             </div>
-            <div class="right-sidebar-item" v-for="(article, index) in relatedPosts" :key="index">
-                <a target="_blank" :href="article.path" :title="article.title">{{article.title}}</a>
-            </div>
-        </div>
-
-        <div class="right-sidebar-links">
-            <div class="right-sidebar-header">
-                想法
-                <a
-                    href="/passages/2019-11-25-how-insist-on-learning/"
-                    target="_blank"
-                >&gt;&gt;&gt;查看全部</a>
-            </div>
             <div
                 class="right-sidebar-item"
-                v-for="(diary, index) in relatedDiaries"
+                v-for="(article, index) in relatedPosts"
                 :key="index"
-                :title="diary.title"
             >
-                <a target="_blank" :href="diary.path" :title="diary.title">{{diary.title}}</a>
+                <a
+                    target="_blank"
+                    :href="article.path"
+                    :title="article.title"
+                    >{{ article.title }}</a
+                >
             </div>
         </div>
-
-        <TOC />
     </aside>
 </template>
 
