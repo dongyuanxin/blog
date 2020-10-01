e9de4eb19efb384b31f1bf2a61c7fd0802bf88de
yuanxindong <yuanxin.me@gmail.com>
2020-10-01 21:19:47 +0800

    feat: 自动读取notes文件

--- .vuepress/config/themeConf.js
+++ .vuepress/config/themeConf.js
@@ -1,109 +1,4 @@
-const sidebar = require("./sidebar-auto");
-
-const docsSidebar = {
-    "/docs/设计模式手册": [
-        {
-            title: "创建型模式",
-            collapsable: true,
-            children: [
-                ["01.创建型模式/01.单例模式.md", "单例模式"],
-                ["01.创建型模式/02.工厂模式.md", "工厂模式"],
-                ["01.创建型模式/03.抽象工厂模式.md", "抽象工厂模式"]
-            ]
-        },
-        {
-            title: "结构型模式",
-            collapsable: true,
-            children: [
-                ["02.结构型模式/01.享元模式.md", "享元模式"],
-                ["02.结构型模式/02.代理模式.md", "代理模式"],
-                ["02.结构型模式/03.桥接模式.md", "桥接模式"],
-                ["02.结构型模式/04.组合模式.md", "组合模式"],
-                ["02.结构型模式/05.装饰者模式.md", "装饰者模式"],
-                ["02.结构型模式/06.适配器模式.md", "适配器模式"]
-            ]
-        },
-        {
-            title: "行为型模式",
-            collapsable: true,
-            children: [
-                ["03.行为型模式/01.命令模式.md", "命令模式"],
-                ["03.行为型模式/02.备忘录模式.md", "备忘录模式"],
-                ["03.行为型模式/03.模板模式.md", "模板模式"],
-                ["03.行为型模式/04.状态模式.md", "状态模式"],
-                ["03.行为型模式/05.策略模式.md", "策略模式"],
-                ["03.行为型模式/06.解释器模式.md", "解释器模式"],
-                ["03.行为型模式/07.订阅-发布模式.md", "订阅-发布模式"],
-                ["03.行为型模式/08.责任链模式.md", "责任链模式"],
-                ["03.行为型模式/09.迭代器模式.md", "迭代器模式"]
-            ]
-        }
-    ]
-};
-
-const noteSidebar = {
-    "/notes/": [
-        {
-            title: "设计模式",
-            collapsable: true,
-            sidebarDepth: 0,
-            path: "/notes/02.设计模式/",
-            children: [
-                {
-                    title: "创建型模式",
-                    collapsable: true,
-                    sidebarDepth: 0,
-                    children: [
-                        {
-                            title: "行为型模型",
-                            collapsable: true,
-                            sidebarDepth: 0,
-                            children: [
-                                {
-                                    title: "策略模式",
-                                    collapsable: true,
-                                    sidebarDepth: 0,
-                                    path:
-                                        "/notes/02.设计模式/1.创建型模式/3.行为型模式/1.策略模式/",
-                                    children: []
-                                },
-                                {
-                                    title: "策略模式",
-                                    collapsable: true,
-                                    sidebarDepth: 0,
-                                    path:
-                                        "/notes/02.设计模式/1.创建型模式/3.行为型模式/1.策略模式/",
-                                    children: []
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    title: "结构型模式",
-                    collapsable: true,
-                    sidebarDepth: 0,
-                    children: [
-                        {
-                            title: "享元模式",
-                            collapsable: true,
-                            sidebarDepth: 0,
-                            path: "/notes/02.设计模式/2.结构型模式/1.享元模式/",
-                            children: []
-                        },
-                        {
-                            title: "代理模式",
-                            collapsable: true,
-                            sidebarDepth: 0,
-                            path: "/notes/02.设计模式/2.结构型模式/2.代理模式/",
-                            children: []
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-};
+const { getSidebar } = require("./../../bin/sidebar");
 
 module.exports = {
     repo: "dongyuanxin/blog",
@@ -113,17 +8,17 @@ module.exports = {
     lastUpdated: "更新于",
     nav: [
         {
+            text: "技能图谱",
+            link: "/notes/"
+        },
+        {
             text: "前端图谱",
             link: "https://xxoo521.com/frontend/"
         },
         {
             text: "算法题解",
             link: "https://xxoo521.com/algorithm/"
-        },
-        {
-            text: "文章归档",
-            link: "https://xxoo521.com/archives/"
         }
     ],
-    sidebar: noteSidebar
+    sidebar: getSidebar()
 };
--- bin/sidebar.js
+++ bin/sidebar.js
@@ -1,120 +1,57 @@
 const fs = require("fs");
 const path = require("path");
-const ejs = require("ejs");
-const logger = require("tracer").colorConsole();
 
-const docsRoot = path.join(__dirname, "..", "docs");
-const notesRoot = path.join(__dirname, "..", "notes");
-const sidebarPath = path.join(
-  __dirname,
-  "..",
-  ".vuepress",
-  "config",
-  "sidebar-auto.js"
-);
-const template = `
-<% for(let variable of variables) { %>
-  const <%- variable.name %> = <%- JSON.stringify(variable.js); %>
-<% } %>
+module.exports.getSidebar = getSidebar;
 
-module.exports = {
-  <% for (let variable of variables) { %>
-    "<%- variable.path %>": <%- variable.name %>,
-  <% } %>
+const ROOT_PATH = path.resolve(process.cwd(), "notes");
+
+function getSidebar() {
+    const toc = [];
+    scan(ROOT_PATH, toc);
+    return {
+        "/notes/": toc
+    };
 }
-`;
 
-main();
-
-/**
- * 主体函数
- */
-function main() {
-  const variables = [];
-
-  const tocs = readTocs(docsRoot);
-  tocs.forEach(toc => {
-    const js = mapTocToSidebar(toc);
-    if (!js.length) {
-      return;
-    }
-
-    variables.push({
-      path: `/docs/${path.basename(toc)}/`,
-      name: path.basename(toc).replace(/ /g, "_"),
-      js
+function scan(parentPath, toc = []) {
+    const folders = fs.readdirSync(parentPath);
+    folders.sort((a, b) => {
+        if (a < b) return -1;
+        else if (a > b) return 1;
+        return 0;
     });
-  });
 
-  // const notesJs = mapTocToSidebar(notesRoot);
-  // if (notesJs.length) {
-  //   variables.push({
-  //     path: "/notes/",
-  //     name: "notes",
-  //     js: notesJs
-  //   });
-  // }
+    for (const folderName of folders) {
+        if (!checkValid(folderName)) {
+            continue;
+        }
 
-  fs.writeFileSync(sidebarPath, ejs.render(template, { variables }));
+        const folderPath = path.resolve(parentPath, folderName);
+        const fileStat = fs.statSync(folderPath);
+        if (fileStat.isFile() && folderName.endsWith(".md")) {
+            toc.push({
+                title: folderName.replace(/^\d+\./, "").replace(/\.md$/, ""),
+                collapsable: true,
+                sidebarDepth: 0,
+                path: `/notes/${path.relative(ROOT_PATH, folderPath)}`,
+                children: []
+            });
+        } else if (fileStat.isDirectory()) {
+            const subToc = {
+                title: folderName.replace(/^\d+\./, ""),
+                collapsable: true,
+                sidebarDepth: 0,
+                children: []
+            };
+            if (fs.existsSync(path.join(folderPath, "readme.md"))) {
+                subToc.path = `/notes/${path.relative(ROOT_PATH, folderPath)}/`;
+            }
+            scan(folderPath, subToc.children);
+            toc.push(subToc);
+        }
+    }
 }
 
-/**
- * 读取指定目录的文件夹作为不同的目录
- * @param {String} root
- */
-function readTocs(root) {
-  const result = [];
-  const files = fs.readdirSync(root);
-  files.forEach(name => {
-    const file = path.resolve(root, name);
-    if (fs.statSync(file).isDirectory()) {
-      result.push(file);
-    }
-  });
-  return result;
-}
-
-/**
- * 将对应目录映射为对应的边栏配置
- * @param {String} root
- * @param {String} prefix
- */
-function mapTocToSidebar(root, prefix) {
-  prefix = prefix || "";
-  let sidebar = [];
-
-  const files = fs.readdirSync(root);
-  files.forEach(filename => {
-    const file = path.resolve(root, filename);
-    const stat = fs.statSync(file);
-    let [order, title, type] = filename.split(".");
-
-    order = parseInt(order, 10);
-    if (isNaN(order) || order < 0) {
-      return;
-    }
-
-    if (sidebar[order]) {
-      logger.warn(
-        `For ${file}, its order has appeared in the same level directory. And it will be rewritten.`
-      );
-    }
-
-    if (stat.isDirectory()) {
-      sidebar[order] = {
-        title,
-        collapsable: false,
-        children: mapTocToSidebar(file, prefix + filename + "/")
-      };
-    } else {
-      if (type !== "md") {
-        logger.error(`For ${file}, its type is not supported.`);
-        return;
-      }
-      sidebar[order] = [prefix + filename, title];
-    }
-  });
-
-  sidebar = sidebar.filter(item => item !== null && item !== undefined);
-  return sidebar;
+function checkValid(name = "") {
+    return /^\d+\./.test(name);
 }
